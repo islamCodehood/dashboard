da5ef3350dc8d11ea027c0dde22a6f4354ef32d8
islamCodehood <islam.sayed8@gmail.com>
2023-07-12 21:55:06 +0300

    remove sidemenu component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import DashboardMenu from './components/dashboard-menu/dashboard-menu.component'
 import Footer from './components/footer/footer.component'
 import Header from './components/header/header.component'
 import Main from './components/main/main.component'
-import SideMenu from './components/side-menu/side-menu.component'
 
 function App() {
     return (
