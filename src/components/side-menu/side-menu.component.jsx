b8522d6727804ed3123f347b8c587f6068722bce
islamCodehood <islam.sayed8@gmail.com>
2023-07-13 13:49:56 +0300

    style component

--- src/components/side-menu/side-menu.component.jsx
+++ src/components/side-menu/side-menu.component.jsx
@@ -1,7 +1,13 @@
+import Filters from '../filters/filters.component'
 import './side-menu.style.css'
 
 const SideMenu = () => {
-    return <aside>aside</aside>
+    return <aside className="side_menu">
+        <h2>Filter</h2>
+        <div>
+            <Filters />
+        </div>
+    </aside>
 }
 
 export default SideMenu
