d5a2bf76053f586b3b0c17cdebfa32b8d19db1c2
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:28:21 +0300

    add Link to navigate different sections

--- src/components/tab/tab.component.jsx
+++ src/components/tab/tab.component.jsx
@@ -1,5 +1,22 @@
-const Tab = () => {
-    return <div>tab</div>
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+const Tab = ({ title, active, toggleActive, id, path }) => {
+    useEffect(() => {
+        console.log(active)
+      }, [active])
+    return (
+        <Link
+        to={path}
+            className={`text-[#1A1A1A] font-semibold px-4 py-2  ${
+                active
+                    ? 'border-[2.5px] border-t-[#4830D9] border-b-0 bg-white'
+                    : 'border-t-[2.5px] bg-[#F3F3F6]'
+            } `}
+            onClick={() => toggleActive(id)}
+        >
+            {title}
+        </Link>
+    )
 }
 
 export default Tab
