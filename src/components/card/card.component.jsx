68c5c9513e86d98e5d5290c852b4df8033f33654
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:21:02 +0300

    first commit

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,26 @@
 import './card.style.css'
-const Card = () => {
+const Card = ({ bg, border, title, list, after, connected }) => {
     return (
         <li className="card_wrapper">
-            <section className="card">card</section>
+            <section className={`w-48 border-2 ${border} rounded-t-md`}>
+                <div className={`p-3 ${bg} h-16`}>
+                    <h3 className="text-white text-lg font-semibold leading-5">
+                        {title} ({list.length})
+                    </h3>
+                </div>
+                <ul className="text-[#1A1A1A]">
+                    {list.map((item, idx) => (
+                        <li
+                            key={idx}
+                            className={`relative mb-1 px-3 py-2 odd:bg-gray-200 text-sm font-semibold connector ${item.connected ? after: ""}`}
+                            
+                        >
+                            {/*  */}
+                            {item.title}
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </li>
     )
 }
