88079f808767cab44874ba5880d33a96c829ade8
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:22:40 +0300

    render tab dynamically

--- src/components/tabs/tabs.component.jsx
+++ src/components/tabs/tabs.component.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import Tab from "../tab/tab.component";
-
+const tabItems = [
+  {id: "1", title: "MITRE", active: true, path: "/"},
+  {id: "2", title: "Custom Base Layer", active: false, path: "/charts"},
+]
 const Tabs = () => {
+  const [tabs, setTabs] = useState(tabItems)
+
+  const toggleActive = (id) => {
+    setTabs(tabs.map(tab => {
+      if (tab.id === id) {
+        return {...tab, active: true}
+      } 
+      return {...tab, active: false}
+    }))
+  }
+
+  useEffect(() => {
+    console.log(tabs)
+  }, [tabs])
   return ( 
-      <ul className="flex">
-        <li><Tab /></li>
-        <li><Tab /></li>
-        <li><Tab /></li>
-      </ul>
+      <div className="flex">
+        {
+          tabs.map(tab => <Tab key={tab.id} toggleActive={toggleActive} {...tab} />)
+        }
+
+      </div>
    );
 }
  
