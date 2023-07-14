75cbeaae6185df75df38f8dae29bc477ad7ede31
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:26:28 +0300

    refactor component to render new components

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,8 +1,17 @@
 import './nav.style.css'
+import { ReactComponent as DownArrow } from './icons/down-arrow.svg';
+import { ReactComponent as MenuVertical } from './icons/menu-vertical.svg';
+
 const Nav = () => {
     return (
-        <nav>
-            <h2>ATT&CK Navigator</h2>
+        <nav className="flex justify-between">
+            <div className="flex gap-6">
+            <h2 className="text-[#1A1A1A] text-xl font-bold">ATT&CK Navigator</h2>
+            <button className="flex justify-between items-center w-28 px-1.5 py-1 bg-white rounded-md shadow-lg font-semibold text-[#1A1A1A]">Enterprise <span className="w-5"><DownArrow /></span></button>
+
+            </div>
+
+            <button className="flex justify-between items-center w-8 px-1.5 py-1 bg-white rounded-md shadow-lg"><MenuVertical /></button>
         </nav>
     )
 }
