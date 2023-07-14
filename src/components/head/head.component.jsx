cdf16c25b0a679c4829981ae2064ca426d6bb354
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:25:17 +0300

    add code to render the head

--- src/components/head/head.component.jsx
+++ src/components/head/head.component.jsx
@@ -3,8 +3,32 @@ import './head.style.css'
 const Head = () => {
     return (
         <div className="head">
-            <div>Logo</div>
-            <h1>CYWARE</h1>
+            <div className="flex">
+                <div className="">
+                    <img
+                        src={process.env.PUBLIC_URL + '/assets/icons/logo.png'}
+                        alt="logo"
+                        className="w-6"
+                    />
+                </div>  
+                <h1 className="text-xl font-semibold text-[#1A1A1A]">CYWARE</h1>
+            </div>
+            <div className='flex gap-6 items-center'>
+                <button className="border-2 border-[#8E83E4] rounded-md py-0.5 px-2 text-[#8E83E4]  font-semibold">＋New</button>
+                <img
+                        src={process.env.PUBLIC_URL + '/assets/icons/bell.png'}
+                        alt="logo"
+                        className="w-6 h-6"
+                    />
+                <img
+                        src={process.env.PUBLIC_URL + '/assets/icons/question.png'}
+                        alt="logo"
+                        className="w-7 h-7"
+                    />
+                    <div className="w-10 h-10 bg-[#E6E9EE] rounded-full flex justify-center items-center text-[#808893] font-semibold">
+                        AK
+                    </div>
+            </div>
         </div>
     )
 }
