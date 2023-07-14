485fd7d28ba5cbf8e289c9a0e0e498070c4876db
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:26:55 +0300

    add input text

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -1,5 +1,16 @@
+import './search.style.css'
 const Search = () => {
-    return <div>Search</div>
+    return (
+        <div>
+            <label className="search">
+                <input
+                    className="border-2 rounded-md h-9 w-[420px] p-2"
+                    type="text"
+                    placeholder="Search Technique / Sub-technique"
+                />
+            </label>
+        </div>
+    )
 }
 
 export default Search
