716136d0bedcbd2c4a57e615444213c8cafd0083
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 15:30:20 +0300

    first commit

---
+++ src/components/dashboard-menu-button/dashboard-menu-button.component.jsx
@@ -0,0 +1,20 @@
+import './dashboard-menu-button.style.css'
+
+const DashboardMenuButton = ({path, active, toggleActiveIcon, id}) => {
+  console.log(path)
+  return ( 
+    <div>
+      <div onClick={() => toggleActiveIcon(id)}  className={`p-1 rounded-sm ${active ? "active": ""}`}>
+                    {/* <img
+                        src={
+                            process.env.PUBLIC_URL + path
+                        }
+                        alt="icon"
+                    /> */}
+                    <div className="w-5 text-white ">{path}</div>
+                </div>
+    </div>
+   );
+}
+ 
+export default DashboardMenuButton;
