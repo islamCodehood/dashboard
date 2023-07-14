b3d476b4e6fb8a54e2cd5ccc719cbd6dfb783dc8
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 15:28:45 +0300

    import svg icons

--- src/components/dashboard-menu/dashboard-menu.component.jsx
+++ src/components/dashboard-menu/dashboard-menu.component.jsx
@@ -1,6 +1,70 @@
+import { useState } from 'react'
 import './dashboard-menu.style.css'
-// import box from 'assets/icons/box.png'
+import DashboardMenuButton from '../dashboard-menu-button/dashboard-menu-button.component'
+import { ReactComponent as Burger } from './icons/burger.svg'
+import { ReactComponent as Certificate } from './icons/certificate.svg'
+import { ReactComponent as Envelope } from './icons/envelope.svg'
+import { ReactComponent as Doc } from './icons/doc.svg'
+import { ReactComponent as Rss } from './icons/rss.svg'
+import { ReactComponent as Twitter } from './icons/twitter.svg'
+import { ReactComponent as Data } from './icons/data.svg'
+import { ReactComponent as Report } from './icons/report.svg'
+
+
+const iconsList = [
+    {
+        id: "1",
+        path: <Burger /> ,
+        active: true,
+    },
+    {
+        id: "2",
+        path: <Certificate /> ,
+        active: false,
+    },
+    {
+        id: "3",
+        path: <Envelope /> ,
+        active: false,
+    },
+    {
+        id: "4",
+        path: <Doc /> ,
+        active: false,
+    },
+    {
+        id: "5",
+        path: <Rss />,
+        active: false,
+    },
+    {
+        id: "6",
+        path: <Twitter />,
+        active: false,
+    },
+    {
+        id: "7",
+        path: <Data />,
+        active: false,
+    },
+    {
+        id: "8",
+        path: <Report />,
+        active: false,
+    },
+]
 const DashboardMenu = () => {
+
+    const [icons, setIcons] = useState(iconsList)
+    const toggleActiveIcon = (id) => {
+        const newIcons = icons.map((icon) => {
+            if (icon.id === id) {
+                return { ...icon, active: true }
+            }
+            return { ...icon, active: false }
+        })
+        setIcons(newIcons)
+    }
     return (
         <aside className="dashboard-side">
             <div className="flex flex-col items-center">
@@ -10,67 +74,14 @@ const DashboardMenu = () => {
                 />
                 <div className="text-white text-[8px] font-bold">CTIX</div>
             </div>
+
+
             <div className="grow flex flex-col gap-6">
-                <div className=" p-1 rounded-sm">
-                    <img
-                        src={
-                            process.env.PUBLIC_URL + '/assets/icons/burger.png'
-                        }
-                        alt="icon"
-                    />
-                </div>
-                <div className=" p-1 rounded-sm">
-                    <img
-                        src={
-                            process.env.PUBLIC_URL +
-                            '/assets/icons/certificate.png'
-                        }
-                        alt="icon"
-                    />
-                </div>
-                <div className=" p-1 rounded-sm">
-                    <img
-                        src={
-                            process.env.PUBLIC_URL +
-                            '/assets/icons/envelope.png'
-                        }
-                        alt="icon"
-                    />
-                </div>
-                <div className=" p-1 rounded-sm">
-                    <img
-                        src={process.env.PUBLIC_URL + '/assets/icons/doc.png'}
-                        alt="icon"
-                    />
-                </div>
-                <div className=" p-1 rounded-sm">
-                    <img
-                        src={process.env.PUBLIC_URL + '/assets/icons/rss.png'}
-                        alt="icon"
-                    />
-                </div>
-                <div className=" p-1 rounded-sm">
-                    <img
-                        src={
-                            process.env.PUBLIC_URL + '/assets/icons/twitter.png'
-                        }
-                        alt="icon"
-                    />
-                </div>
-                <div className="bg-[#4830D9] p-1 rounded-md">
-                    <img
-                        src={process.env.PUBLIC_URL + '/assets/icons/data.png'}
-                        alt="icon"
-                    />
-                </div>
-                <div className=" p-1 rounded-sm">
-                    <img
-                        src={
-                            process.env.PUBLIC_URL + '/assets/icons/report.png'
-                        }
-                        alt="icon"
-                    />
-                </div>
+                {
+                    icons.map((icon, idx) => 
+                    <DashboardMenuButton key={idx} {...icon} toggleActiveIcon={toggleActiveIcon} />
+                    )
+                }
             </div>
             <div className="flex">
                 <div>
