3f82b81f5e252a1c71eee7f6d17ee040d7b938e7
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:25:56 +0300

    refactor main component sections

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -2,12 +2,27 @@ import './main.style.css'
 
 import SideMenu from '../side-menu/side-menu.component'
 import Cards from '../cards/cards.component'
+import Tabs from '../tabs/tabs.component'
+import Search from '../search/search.component'
+import CardsOptions from '../cards-options/cards-options.component'
+import { Routes, Route } from 'react-router-dom'
+import Charts from '../charts/charts'
 
 const Main = () => {
     return (
         <main>
             <SideMenu />
-            <Cards />
+            <div className="options-wrapper">
+                <Tabs />
+                <Search />
+            </div>
+            <div className="cards-options-wrapper">
+                <CardsOptions />
+            </div>
+            <Routes>
+                <Route path="/" element={<Cards  />} />
+				<Route path="/charts" element={<Charts />} />
+            </Routes>
         </main>
     )
 }
