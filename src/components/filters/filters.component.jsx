5fa83ffb2f4b16a1f244086d132a647e5acd1690
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:24:47 +0300

    add code to update state from child

--- src/components/filters/filters.component.jsx
+++ src/components/filters/filters.component.jsx
@@ -1,8 +1,9 @@
 import Filter from '../filter/filter.component'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const filterItems = [
     {
+        open: false,
         id: '1',
         title: 'Sources',
         options: [
@@ -10,13 +11,21 @@ const filterItems = [
             { title: 'Source 2' },
             { title: 'Source 3' },
             { title: 'Source 4' },
+            { title: 'Source 5' },
+            { title: 'Source 6' },
+            { title: 'Source 7' },
+            { title: 'Source 8' },
+            { title: 'Source 9' },
+            { title: 'Source 10' },
+            { title: 'Source 11' },
+            { title: 'Source 12' },
         ],
     },
     {
+        open: false,
         id: '2',
         title: 'System Created Date',
         options: [
-            { title: 'Select All' },
             { title: 'Source 1' },
             { title: 'Source 2' },
             { title: 'Source 3' },
@@ -24,10 +33,10 @@ const filterItems = [
         ],
     },
     {
+        open: false,
         id: '3',
         title: 'System Modified Date',
         options: [
-            { title: 'Select All' },
             { title: 'Source 1' },
             { title: 'Source 2' },
             { title: 'Source 3' },
@@ -35,10 +44,10 @@ const filterItems = [
         ],
     },
     {
+        open: false,
         id: '4',
         title: 'Platforms',
         options: [
-            { title: 'Select All' },
             { title: 'Source 1' },
             { title: 'Source 2' },
             { title: 'Source 3' },
@@ -46,10 +55,10 @@ const filterItems = [
         ],
     },
     {
+        open: false,
         id: '5',
         title: 'Mire Threat Actors',
         options: [
-            { title: 'Select All' },
             { title: 'APT29' },
             { title: 'Lazarus Group' },
             { title: 'APT23' },
@@ -61,10 +70,10 @@ const filterItems = [
         ],
     },
     {
+        open: false,
         id: '6',
         title: 'Softwares',
         options: [
-            { title: 'Select All' },
             { title: 'Source 1' },
             { title: 'Source 2' },
             { title: 'Source 3' },
@@ -72,10 +81,10 @@ const filterItems = [
         ],
     },
     {
+        open: false,
         id: '7',
         title: 'Log Data Sources',
         options: [
-            { title: 'Select All' },
             { title: 'Source 1' },
             { title: 'Source 2' },
             { title: 'Source 3' },
@@ -83,10 +92,10 @@ const filterItems = [
         ],
     },
     {
+        open: false,
         id: '8',
         title: 'Mitigations',
         options: [
-            { title: 'Select All' },
             { title: 'Source 1' },
             { title: 'Source 2' },
             { title: 'Source 3' },
@@ -94,15 +103,25 @@ const filterItems = [
         ],
     },
 ]
-console.log(filterItems)
 
 const Filters = () => {
     const [filters, setFilters] = useState(filterItems)
+    const toggleOpen = (id) => {
+        console.log(id)
+        const newFilters = filters.map((filterItem) => {
+            if (filterItem.id === id && !filterItem.open ) {
+                return { ...filterItem, open: true }
+            }
+            return { ...filterItem, open: false }
+        })
+        setFilters(newFilters)
+    }
+
     return (
         <ul className="flex flex-col gap-2">
-            {filterItems.map((item) => (
+            {filters.map((item) => (
                 <li key={item.id}>
-                    <Filter {...item} />
+                    <Filter toggleOpen={toggleOpen} {...item} />
                 </li>
             ))}
         </ul>
