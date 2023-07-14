38e5ebea81300e63a39c41b96006fa176edbbc89
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 10:23:59 +0300

    add filter content

--- src/components/filter/filter.component.jsx
+++ src/components/filter/filter.component.jsx
@@ -1,13 +1,48 @@
-import { ReactComponent as DownArrow } from './icons/down-arrow.svg';
-import { ReactComponent as RightArrow } from './icons/right-arrow.svg';
+// import { ReactComponent as DownArrow } from './icons/down-arrow.svg'
+import { ReactComponent as RightArrow } from './icons/right-arrow.svg'
+import { useEffect } from 'react'
 
-const Filter = ({title, options}) => {
-    return <div className="border-2 h-12 px-2 py-2 flex justify-between">
-      <div className="text-[#1A1A1A]">{title}</div>
-      <div className="w-6">
-        <RightArrow />
-      </div>
-    </div>
+const Filter = ({ title, options, toggleOpen, open, id }) => {
+
+    return (
+        <div
+            onClick={() => toggleOpen(id)}
+            className={`border-2  py-2 h-12 overflow-y-hidden duration-500 ${
+                open ? ' h-[250px]' : 'h-12'
+            }`}
+        >
+            <div className=" flex justify-between mb-4 px-2">
+                <div className="text-[#1A1A1A]">{title}</div>
+                <button
+                    className={`w-6 duration-200 ${
+                        open ? 'rotate-90' : 'rotate-0'
+                    }`}
+                >
+                    <RightArrow />
+                </button>
+            </div>
+            <div className="px-3 border-b-2 py-1.5 mb-3">
+                <label className="">
+                    <input type="checkbox" name="" value="Select All" />
+                    <span className="ml-2">Select All</span>
+                </label>
+            </div>
+            <ul className={`styled-scrollbar duration-500  ${open ? "h-[150px] overflow-y-auto" : "h-0"}`}>
+                {options.map((option, idx) => (
+                    <li key={idx} className="px-3">
+                        <label className="">
+                            <input
+                                type="checkbox"
+                                name=""
+                                value={option.title}
+                            />
+                            <span className="ml-2">{option.title}</span>
+                        </label>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Filter
